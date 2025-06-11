c7f157eba376126dc464b1928b1ae5ffd7f889bf
Jakub Bober <kuba.bober707@gmail.com>
2025-06-11 14:41:04 +0200

    Add Bluetooth handling

--- ESP32-Matrix-Driver/ESP32-Matrix-Driver.ino
+++ ESP32-Matrix-Driver/ESP32-Matrix-Driver.ino
@@ -1,14 +1,26 @@
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-#include <avr/power.h>  // Required for 16 MHz Adafruit Trinket
+#include <BluetoothSerial.h>
+#include <ArduinoJson.h>
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
+
+// Check Serial Port Profile
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Port Profile for Bluetooth is not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+BluetoothSerial SerialBT;
+String deviceName = "ESP32 Matrix Controller";
+
 
 #define PIN 16
 #define NUMPIXELS 256
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-#define DELAYVAL 100  // Time (in milliseconds) to pause between pixels
+#define DELAYVAL 100
 
 int getFlippedIndex(int x, int y) {
   if (y % 2 == 0) {
@@ -18,12 +30,36 @@ int getFlippedIndex(int x, int y) {
   }
 }
 
+JsonDocument doc;
+
+
 void setup() {
   pixels.begin();
   pixels.setBrightness(10);
+  Serial.begin(115200);
+  SerialBT.begin(deviceName);
+  Serial.printf("\"%s\" is started. You can now pair it with Bluetooth!\n", deviceName.c_str());
 }
 
 void loop() {
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+    const char* json = "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}"; 
+
+    DeserializationError error = deserializeJson(doc, json);
+    if (error) {
+      Serial.print("deserializeJson() returned ");
+      Serial.println(error.c_str());
+      return;
+    } else {
+      const char* sensor = doc["sensor"];
+      Serial.println(sensor);
+    }
+  }
+
   pixels.clear();
 
   for (int y = 0; y < 16; y++) {
